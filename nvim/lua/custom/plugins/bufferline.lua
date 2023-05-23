b4adb5f0896ed415ba1c2bf124d95000c133dde2
ハチ <82988679+pichayakorn@users.noreply.github.com>
2023-05-23 15:33:53 +0700

    feat(nvim, tmux): backup

---
+++ nvim/lua/custom/plugins/bufferline.lua
@@ -0,0 +1,35 @@
+return {
+  'akinsho/nvim-bufferline.lua',
+  opts = {
+    options = {
+      mode = "tabs",
+      separator_style = 'slant',
+      always_show_bufferline = false,
+      show_buffer_close_icons = false,
+      show_close_icon = false,
+      color_icons = true
+    },
+    highlights = {
+      separator = {
+        fg = '#073642',
+        bg = '#002b36',
+      },
+      separator_selected = {
+        fg = '#073642', },
+      background = {
+        fg = '#657b83',
+        bg = '#002b36'
+      },
+      buffer_selected = {
+        fg = '#fdf6e3',
+        bold = true,
+      },
+      fill = {
+        bg = '#073642'
+      },
+    }
+  },
+  -- Key map
+  vim.keymap.set('n', '<Tab>', '<Cmd>BufferLineCycleNext<CR>', {}),
+  vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferLineCyclePrev<CR>', {})
+}
